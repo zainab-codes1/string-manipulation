3e82b254fe7c4d882a65690dd46ac11f48e4dc03
zainab-codes1 <157022676+zainab-codes1@users.noreply.github.com>
2024-03-21 15:25:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
-        var originalText = "";
-        var lastModifiedText = "";
-        var history = [];
+var originalText = "";
+var lastModifiedText = "";
+var textHistory = [];
 
-        function updateOutput() {
-            document.getElementById("output").innerText = "Modified Text: " + lastModifiedText;
-        }
+function updateOutput() {
+    document.getElementById("output").innerText = lastModifiedText;
+}
 
-        function upperCase() {
-            originalText = document.getElementById("inputText").value;
-            lastModifiedText = originalText.toUpperCase();
-            history.push(originalText);
-            updateOutput();
-        }
+function convertToUppercase() {
+    if (lastModifiedText !== "") {
+        lastModifiedText = lastModifiedText.toUpperCase();
+    } else {
+        originalText = document.getElementById("inputText").value;
+        lastModifiedText = originalText.toUpperCase();
+    }
+    textHistory.push(lastModifiedText);
+    updateOutput();
+}
 
-        function lowerCase() {
-            originalText = document.getElementById("inputText").value;
-            lastModifiedText = originalText.toLowerCase();
-            history.push(originalText);
-            updateOutput();
-        }
+function convertToLowercase() {
+    if (lastModifiedText !== "") {
+        lastModifiedText = lastModifiedText.toLowerCase();
+    } else {
+        originalText = document.getElementById("inputText").value;
+        lastModifiedText = originalText.toLowerCase();
+    }
+    textHistory.push(lastModifiedText);
+    updateOutput();
+}
 
-        function reverse() {
-            originalText = document.getElementById("inputText").value;
-            lastModifiedText = originalText.split('').reverse().join('');
-            history.push(originalText);
-            updateOutput();
-        }
+function reverseText() {
+    if (lastModifiedText !== "") {
+        lastModifiedText = lastModifiedText.split('').reverse().join('');
+    } else {
+        originalText = document.getElementById("inputText").value;
+        lastModifiedText = originalText.split('').reverse().join('');
+    }
+    textHistory.push(lastModifiedText);
+    updateOutput();
+}
 
-        function change() {
-            var index = parseInt(prompt("Enter the index of the letter to change:"));
-            var newLetter = prompt("Enter the new letter:");
-            originalText = document.getElementById("inputText").value;
-            if (index >= 0 && index < originalText.length) {
-                lastModifiedText = originalText.substring(0, index) + newLetter + originalText.substring(index + 1);
-                history.push(originalText);
-                updateOutput();
-            } else {
-                alert("Invalid index.");
-            }
-        }
+function changeLetter() {
+    var inputText = document.getElementById("inputText").value;
+    var textToReplace = document.getElementById("textToReplace").value;
+    var replacementText = document.getElementById("replacementText").value;
+    var regex = new RegExp(textToReplace, 'gi');
 
-        function undo() {
-            if (history.length > 0) {
-                lastModifiedText = history.pop();
-                updateOutput();
-            } else {
-                alert("No changes to undo.");
-            }
-        }
+    if (lastModifiedText !== "") {
+        lastModifiedText = inputText.replace
+    (regex, replacementText);
+    } else {
+        lastModifiedText = lastModifiedText.replace
+    (regex, replacementText);
+    }
+
+    textHistory.push(lastModifiedText);
+    updateOutput();
+}
+
+function undoChange() {
+    if (textHistory.length > 1) {
+        textHistory.pop(); // Remove the last change
+        lastModifiedText = textHistory[textHistory.length - 1]; // Get the previous state
+        updateOutput();
+    } else if (textHistory.length === 1) {
+        lastModifiedText = originalText;
+        textHistory.pop();
+        updateOutput();
+    } else {
+        alert("No changes to undo.");
+    }
+}
